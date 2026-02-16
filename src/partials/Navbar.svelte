2644f042c12c7f8f5453f7b475be8ab19c32237a
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2026-02-16 11:21:02 +0700

    [fix] navbar

--- src/partials/Navbar.svelte
+++ src/partials/Navbar.svelte
@@ -2,6 +2,8 @@
   import { writable, type Writable } from 'svelte/store';
 	import { Icon } from 'svelte-icons-pack';
 	import { BsList, BsX } from 'svelte-icons-pack/bs';
+	import { RiBusinessCalendar2Line, RiBusinessMailLine, RiLogosFacebookCircleLine, RiLogosInstagramLine, RiLogosTwitterXFill, RiLogosWhatsappLine, RiLogosYoutubeLine } from 'svelte-icons-pack/ri';
+	import { formatDate } from '@/lib/datetime';
 
   const isShowMenu: Writable<Boolean> = writable(false);
 
@@ -10,19 +12,81 @@
   }
 </script>
 
-<div id="top" class="top-0 z-99 fixed w-full h-20 flex flex-row items-center justify-center py-auto bg-white shadow-lg">
-  <div class="px-5 md:px-0 container max-w-6xl flex flex-row items-center justify-between mx-auto">
+<div id="top" class="top-0 z-99 fixed w-full flex flex-col items-center justify-center py-auto bg-white shadow-lg">
+  <div class="hidden md:flex h-8 w-full bg-qc-darker">
+    <div class="container max-w-6xl mx-auto">
+      <div class="w-full h-full flex flex-row justify-between items-center text-xs text-qc-white">
+        <div class="flex flex-row items-center">
+          <div class="px-2.5 border-x-white/20 border-x h-8 flex flex-row items-center gap-2">
+            <Icon
+              src={RiBusinessCalendar2Line}
+              size="16"
+              className="fill-qc-white "
+            />
+            <span>{formatDate(new Date().toISOString())}</span>
+          </div>
+          <a href="mailto:info@qurancenter.net" class="px-2.5 border-r-white/20 border-r h-8 flex flex-row items-center gap-2 group hover:text-qc-melon">
+            <Icon
+              src={RiBusinessMailLine}
+              size="16"
+              className="fill-qc-white group-hover:fill-qc-melon"
+            />
+            <span>info@qurancenter.net</span>
+          </a>
+          <a href="https://wa.me/6281285960701" class="px-2.5 border-r-white/20 border-r h-8 flex flex-row items-center gap-2 group hover:text-qc-melon">
+            <Icon
+              src={RiLogosWhatsappLine}
+              size="16"
+              className="fill-qc-white group-hover:fill-qc-melon"
+            />
+            <span>(+62) 812-8596-0701</span>
+          </a>
+        </div>
+        <div class="flex flex-row items-center border-collapse">
+          <a href="/" class="px-2.5 border-x-white/20 border-x h-8 flex items-center group">
+            <Icon
+              src={RiLogosFacebookCircleLine}
+              size="16"
+              className="fill-qc-white group-hover:fill-qc-melon"
+            />
+          </a>
+          <a href="/" class="px-2.5 border-r-white/20 border-r h-8 flex items-center group">
+            <Icon
+              src={RiLogosInstagramLine}
+              size="16"
+              className="fill-qc-white group-hover:fill-qc-melon"
+            />
+          </a>
+          <a href="/" class="px-2.5 border-r-white/20 border-r h-8 flex items-center group">
+            <Icon
+              src={RiLogosYoutubeLine}
+              size="16"
+              className="fill-qc-white group-hover:fill-qc-melon"
+            />
+          </a>
+          <a href="/" class="px-2.5 border-r-white/20 border-r h-8 flex items-center group">
+            <Icon
+              src={RiLogosTwitterXFill}
+              size="16"
+              className="fill-qc-white group-hover:fill-qc-melon"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="h-16 px-5 md:px-0 container max-w-6xl flex flex-row items-center justify-between mx-auto">
     <div class="flex flex-row gap-3 items-center">
       <a href="/" class="w-fit h-fit">
         <img
           src="/icons/logo-text.svg"
           alt="Quran Center"
-          class="h-12 w-auto"
+          class="h-auto w-40"
         />
       </a>
     </div>
-    <nav class="hidden md:flex flex-row gap-8 items-center justify-center w-fit font-semibold">
-      <a href="/#jumbotron" class="hover:text-qc">Beranda</a>
+    <nav class="hidden md:flex flex-row gap-8 items-center justify-center w-fit font-medium">
+      <a href="/" class="hover:text-qc">Beranda</a>
       <a href="/#tentang" class="hover:text-qc">Tentang</a>
       <a href="/#layanan" class="hover:text-qc">Layanan</a>
       <a href="/#khutbah" class="hover:text-qc">Khutbah</a>
@@ -31,9 +95,9 @@
     </nav>
     <a
       href="https://wa.me/6281285960701"
-      class="md:block hidden w-fit h-fit cursor-pointer py-3 px-6 rounded-full text-qc-white bg-qc-darker hover:bg-qc text-sm"
+      class="md:block hidden w-fit h-fit cursor-pointer py-2 px-6 rounded-full text-qc-white bg-qc-darker hover:bg-qc text-sm"
     >
-      Konsultasi Gratis
+      Donasi
     </a>
     <div class="flex md:hidden">
       <button
@@ -50,18 +114,21 @@
 </div>
 
 <aside class="{$isShowMenu ? 'translate-y-0' : ''}
+  top-16
   fixed z-98 transition-all
   duration-300
   ease-in-out
   bg-white
+  shadow-lg
   w-full h-fit
   -translate-y-full
   ">
   <div class="flex flex-col gap-3 py-5 justify-center items-center font-medium">
-    <a href="/#top" class="hover:text-qc" onclick={closeMenu}>Beranda</a>
-    <a href="/#tentang" class="hover:text-qc" onclick={closeMenu}>Tentang Kami</a>
+    <a href="/" class="hover:text-qc" onclick={closeMenu}>Beranda</a>
+    <a href="/#tentang" class="hover:text-qc" onclick={closeMenu}>Tentang</a>
     <a href="/#layanan" class="hover:text-qc" onclick={closeMenu}>Layanan</a>
-    <a href="/#proses-halal" class="hover:text-qc" onclick={closeMenu}>Proses Halal</a>
+    <a href="/#khutbah" class="hover:text-qc" onclick={closeMenu}>Khutbah</a>
+    <a href="/posts" class="hover:text-qc" onclick={closeMenu}>Berita</a>
     <a href="/#kontak" class="hover:text-qc" onclick={closeMenu}>Kontak</a>
   </div>
 </aside>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -5,7 +5,7 @@
 	let { children } = $props();
 </script>
 
-<div class="bg-white text-qc-black top-0 mt-20 bottom-0 left-0 right-0 overflow-x-hidden">
+<div class="bg-white text-qc-black top-0 mt-24 bottom-0 left-0 right-0 overflow-x-hidden">
 	<div class="w-full h-full flex flex-col max-w-full">
 		<Navbar />
 		<main class="flex flex-col left-0 right-0 h-auto">
